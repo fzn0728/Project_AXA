e1d5f38b329ca54cbec22e9b0f4d429b5c8a880b
Chandler Fang <thames.ft@gmail.com>
2017-01-29 18:35:53 -0800

    wrap up

--- main.py
+++ main.py
@@ -11,12 +11,28 @@ import numpy as np
 import datetime
 import dateutil.relativedelta
 import matplotlib.pyplot as plt
+import pandas_datareader.data as wb
+import matplotlib
+%matplotlib qt
+
+
+def vol(returns):
+    # Return the standard deviation of returns
+    return np.std(returns)
+    
+def information_ratio(returns, benchmark):
+    diff = returns - benchmark
+    return np.mean(diff) / vol(diff)    
+    
+    
+    
 
 if __name__ == '__main__':
     ### Set working directory
     os.getcwd()
     file_path = r'C:/Users/Chandler/Desktop/application/Interview/AXA/Project_AXA'
     os.chdir(file_path)
+    
     ### Import the data and clean the format
     ROA_df = pd.read_csv('ROA.csv',sep='\s+')
     ROA_df['datadate'] = pd.to_datetime(ROA_df['datadate'],format="%d%b%Y")
@@ -85,7 +101,75 @@ if __name__ == '__main__':
 
     ### Plot the Cummulative Return
     plt.style.use('fivethirtyeight')
+    a = portfolio_return['cum_return'].plot()
+    plt.legend(prop={'size':12})
+    plt.xlabel('Year')
+    plt.ylabel('Cumulative Return')
+    plt.title('Cumulative Return of All Market Portfolio')    
+    
+    
+    ### Get the Benchmark Data
+    SP500_df = pd.read_csv('SP500.csv',index_col=0)
+    SP500_df.index = pd.to_datetime(SP500_df.index,format="%Y%m%d")
+    SP500_df['cum_sp_equal_ret'] = (SP500_df['ewretx']+1).cumprod()-1
+    portfolio_return['cum_sp_equal_ret'] = SP500_df['cum_sp_equal_ret'].values
+    ### Calculate the Information Ratio
+    IC = information_ratio(portfolio_return['cum_return'].values,portfolio_return['cum_sp_equal_ret'].values)
+    
+    ### Rolling vol
+    rolling_vol = pd.rolling_std(portfolio_return[['cum_return','cum_sp_equal_ret']],window=12,min_periods=12)
+    rolling_vol.plot()
+    plt.legend(prop={'size':12})
+    plt.xlabel('Year')
+    plt.ylabel('Volatility')
+    plt.title('12 Months Rolling Volatility of Portfolio Returns and SP500')   
+    
+    ### If industrial neutral
+    merged_df['percentile_ind']=merged_df.groupby(['notified_datadate','sic2'])['ROA'].rank(pct=True) # Calculate the ROA percentile across the same notified date
+    portfolio_ind_return_top = merged_df.groupby('notified_datadate').apply(lambda x: x[x['percentile_ind']>=0.8].mean()) # calcualte top 20% portfolio return 
+    portfolio_ind_return_bottom = merged_df.groupby('notified_datadate').apply(lambda x: x[x['percentile_ind']<=0.2].mean()) # calcualte the bottom 20% portfolio return    
+    
+    portfolio_ind_return = pd.concat([portfolio_ind_return_top[['ret','ROA','percentile_ind']],portfolio_ind_return_bottom[['ret','ROA','percentile_ind']]],axis=1)
+    portfolio_ind_return.columns=['ret_top','ROA_top','percentile_top','ret_bottom','ROA_bottom','percentile_bottom'] # Change the column name
+    portfolio_ind_return['combined_ret'] = portfolio_ind_return['ret_top']-portfolio_ind_return['ret_bottom']    
+    portfolio_ind_return['cum_return'] = (portfolio_ind_return['combined_ret']+1).cumprod()-1
+    portfolio_ind_return['cum_sp_equal_ret'] = SP500_df['cum_sp_equal_ret'].values
+    
+    ### Plot Cumulative Return Comparison
+    compare_df = pd.concat([portfolio_return[['cum_return']],portfolio_ind_return[['cum_return']]],axis=1)
+    compare_df.columns = ['All Market Portfolio','Industrial Neutral Portfolio']
+    compare_df.plot()
+    plt.legend(prop={'size':12})
+    plt.xlabel('Year')
+    plt.ylabel('Cumulative Return')
+    plt.title('Cumulative Return of All Market Portfolio and Industrial Neutral Portfolio')
+    
+    ### Calculate the Information Ratio
+    IC_ind = information_ratio(portfolio_ind_return['cum_return'].values,portfolio_ind_return['cum_sp_equal_ret'].values)    
+    
+    ### Calculate Beta
+    SPY_df = pd.read_csv('SPY.csv',index_col=0)
+    portfolio_ind_return.ix[19:,'SPY_ret'] = SPY_df.ix[1:,'Return'].values
+    cov_matrix = portfolio_ind_return.ix[19:,['combined_ret','SPY_ret']].rolling(window=24,min_periods=24).cov(portfolio_ind_return.ix[19:,['combined_ret','SPY_ret']],pairwise=True)
+    
+    beta_df = pd.DataFrame(index = portfolio_ind_return.index[19:], columns = ['Beta'])
+    for i in range(0,len(beta_df.index)-23):
+        beta_df.ix[i+23,'Beta'] = cov_matrix.iloc[i+23].iloc[0][1]/cov_matrix.iloc[i+23].iloc[1][1]
+    
+    beta_df.plot()
+    plt.legend(prop={'size':12})
+    plt.xlabel('Year')
+    plt.ylabel('Beta Value')
+    plt.title('24 Months Rolling Beta with SPY')    
+    
+    
+    
+    
+    df.plot(portfolio_return['cum_return'].index,portfolio_return[['cum_return']])
 
+
+
+    portfolio_return['cum_return'].plot()
 
 
     merged_df.loc[1,'datadate_y']
